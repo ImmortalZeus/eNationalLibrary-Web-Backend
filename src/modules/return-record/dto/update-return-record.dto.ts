d7e7859408e5182f4cc2757d626a9638835975ec
TranSon2707 <trantienson2707@gmail.com>
2026-06-03 02:06:55 +0700

    record retyrn

--- src/modules/return-record/dto/update-return-record.dto.ts
+++ src/modules/return-record/dto/update-return-record.dto.ts
@@ -4,7 +4,7 @@ import { IsPresent } from 'src/common/validators/isPresent.validator';
 import { IsString } from 'class-validator';
 
 export class UpdateReturnRecordDto extends PartialType(
-    OmitType(CreateReturnRecordDto, ['returnRecordId'] as const)
+    OmitType(CreateReturnRecordDto, ['readerId', 'bookId'] as const)
 ) {
     // No additional fields for now, but you can add any update-specific fields here if needed
     // @IsPresent()
